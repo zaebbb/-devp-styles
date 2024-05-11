8917b91b75348747d244f5f37a9f1e1352a8f48e
zaebbb <elik@example.com>
2024-05-11 17:15:04 +0500

    Patch fixed import type Mods and type Additional

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { classNames } from './lib/classNames'
+import { type Mods, type Additional } from './types/classNames'
 
 export {
   classNames,
+  type Mods,
+  type Additional,
 }
 
 export default {
